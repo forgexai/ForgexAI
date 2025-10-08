9b23ca3383b7d8d13b837bb8ee90380b1e659165
Arnav khajuria <arnavk1802@gmail.com>
2025-10-08 19:16:26 +0530

    privy config update

--- lib/providers/ClientProvider.tsx
+++ lib/providers/ClientProvider.tsx
@@ -1,27 +1,29 @@
 "use client";
 
-import { Toaster } from "@/components/ui/sonner";
+import React from "react";
 import { PrivyProvider } from "@privy-io/react-auth";
-import { ReactNode } from "react";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
 
-function ClientProvider({ children }: { children: ReactNode }) {
+function ClientProvider({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
+      clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID || ""}
       config={{
+        loginMethods: ["wallet","google", "email"],
         appearance: {
           theme: "dark",
-          accentColor: "#8b5cf6",
+          accentColor: "#ff6b35",
+          walletChainType: "solana",  
+          walletList: ["phantom", "backpack", "solflare"],
         },
-        loginMethods: ["google", "wallet"],
-        embeddedWallets: {
-          createOnLogin: "users-without-wallets",
-          requireUserPasswordOnCreate: false,
-          noPromptOnSignature: false,
+        externalWallets: {
+          solana: {
+            connectors: toSolanaWalletConnectors(),
+          },
         },
       }}
     >
-      <Toaster position="top-right" duration={2000} />
       {children}
     </PrivyProvider>
   );
